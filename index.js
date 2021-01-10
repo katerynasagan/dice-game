7d571fb5c5bfa12163c988a91dc408959219752d
katerynasagan <73867153+katerynasagan@users.noreply.github.com>
2021-01-09 23:32:48 -0500

    Add the dice game challenge initial files

---
+++ index.js
@@ -0,0 +1,18 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDice1 = "images/dice" + randomNumber1 + ".png";
+
+document.querySelector(".img1").setAttribute("src", randomDice1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDice2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".img2").setAttribute("src", randomDice2);
+
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins🚩!";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
